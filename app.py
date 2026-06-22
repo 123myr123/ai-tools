997195c91f6b05177413ec77d2be00758a4698eb
myr <you@example.com>
2026-06-22 11:30:09 +0500

    модуль памяти и фикс инструментов

--- app.py
+++ app.py
@@ -8,26 +8,51 @@ def print_fragment(fragment, round_index=0):
     # Setting a default value means the callback is also
     # compatible with .complete() and .respond().
     print(fragment.content, end="", flush=True)
+
 print("Выберите профиль из перечисленых")
 for x in read_folder("system_file/profile"):
     print(x)
 pyti = input("Выберите профиль: ")
+
+def memory_write(content:str):
+     """Writing to the memory module. Entries are preserved between sessions."""
+
+     y = ("system_file/profile/" +pyti)
+     h = y + "/"
+     pyti_config = h + "memory.txt"
+     file = open(pyti_config,'w', encoding='utf-8')
+     file.write(content)
+     file.close
+     return "The information has been recorded."
+
+def memory_read():
+    """reads the memory module"""
+    y = ("system_file/profile/" +pyti)
+    h = y + "/"
+    pyti_config = h + "memory.txt"
+    with open(pyti_config, "r", encoding='utf-8') as file:
+        return file.read()
+
+
+logging.info("выбран профиль " +pyti)
 for config_file in read_folder("system_file/profile/" +pyti):
     y = ("system_file/profile/" +pyti)
     h = y + "/"
     pyti_config = h + config_file
     if config_file == "promt.txt":
         chat = lms.Chat(read_file(pyti_config))
+        logging.info("загружен системный промт")
     if read_file(pyti_config) == "yes":
-        tools = [create_file,read_file,write_file,read_folder,create_folder,run_comand]
+        tools = [create_file,read_file,write_file,read_folder,create_folder,run_comand,memory_read,memory_write]
+        logging.info("загружены инструменты")
 SERVER_API_HOST = "localhost:1234"
 lms.configure_default_client(SERVER_API_HOST)
 model = lms.llm()
 while True:
     mode = int(input("Выберите режим: 1 инструменты, 2 Фото     "))
     if mode == 1:
+        logging.info("Выброн режим работы с инструментами")
         while True:
-    
             try:
                 user_input = input("сообщение:")
             except EOFError:
@@ -44,7 +69,8 @@ while True:
                 on_prediction_fragment=print_fragment,
             )
             print()
-    else:
+    elif mode == 2:
+        logging.info("загружен режим фото")
         image_path = input("Введите путь к фото:") # Replace with the path to your image
         image_handle = lms.prepare_image(image_path)
         user_input = input("cообщение:")
--- test/test.py
+++ test/test.py
@@ -5,14 +5,31 @@ import logging
 import json
 from system_file.tools import *
 from pathlib import Path
+import lmstudio  as lms
+from datetime import datetime
 
 print("Выберите профиль из перечисленых")
 for x in read_folder("system_file/profile"):
     print(x)
 pyti = input("Выберите профиль: ")
-for config_file in read_folder("system_file/profile/" +pyti):
+
+def memory_write(content:str):
+     """Writing to the memory module. Entries are preserved between sessions."""
+
+     y = ("system_file/profile/" +pyti)
+     h = y + "/"
+     pyti_config = h + "memory.txt"
+     file = open(pyti_config,'w', encoding='utf-8')
+     file.write(content)
+     file.close
+     return "The information has been recorded."
+
+def memory_read():
+    """reads the memory module"""
     y = ("system_file/profile/" +pyti)
     h = y + "/"
-    pyti_config = h + config_file
-    if config_file == "promt.txt":
-        print(read_file(pyti_config)) 
+    pyti_config = h + "memory.txt"
+    with open(pyti_config, "r", encoding='utf-8') as file:
+        return file.read()
+
+print(memory_read())
