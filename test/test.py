60ca74b2798bc7b1849975f50b13820efcb71191
myr <you@example.com>
2026-07-06 14:21:11 +0500

    - мусор

--- test/test.py
+++ test/test.py
@@ -1,22 +1,13 @@
 # Это тестовый файл
-import lmstudio as lms
+import requests
+from bs4 import BeautifulSoup
 
-# Create a chat with an initial system prompt.
-model = lms.llm()
-pyti = "system"
-def memory_read():
-    """reads the memory module"""
-    y = ("system_file/profile/" +pyti)
-    h = y + "/"
-    pyti_config = h + "memory.txt"
-    with open(pyti_config, "r", encoding='utf-8') as file:
-        return file.read()
-    
-chat = lms.Chat.from_history({"messages": [
-  { "role": "system", "content": "ты ии асистент все ответы должны быть на руском. Самое первое сообшение имееюшие тип memory:(текст) евляется твоей памятью между чатами" },
-]})
-chat.add_user_message("memory: " + memory_read())
-# Build the chat context by adding messages of relevant types.
-chat.add_user_message("Привет, скажи мне что у тебя в памяте")
-result = model.respond(chat)
-print(result)
+
+
+url = input("Вставте ссылку: ")
+response = requests.get(url)
+response.raise_for_status()
+print(response.text)
+soup = BeautifulSoup(response.text, 'lxml')
+text = soup.get_text()
+print(text)
