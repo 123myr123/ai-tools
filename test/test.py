aa57b938681eaf416613fbfaa58f801566819b17
myr <you@example.com>
2026-07-16 16:04:57 +0500

    попытки в web 2

--- test/test.py
+++ test/test.py
@@ -1,1 +1,5 @@
 
+import json
+data = {"name": "Joe", "age": 25}
+with open("histori.json", "w") as f:
+    json.dump(data, f)
--- web/app.py
+++ web/app.py
@@ -1,12 +1,31 @@
+
+
 from flask import Flask, request, jsonify, render_template
 import lmstudio as lms
+import json
 app = Flask(__name__)
 
 model = lms.llm()
-chat_ai = lms.Chat()
+ai = {
+     'messages': [ 
+           {'role': 'system', 'content': 'Я — AI-ассистент. Все ответы должны быть на русском'}, 
+     ]
+}
+with open("histori.json", "w",encoding='utf-8') as f:
+        json.dump(ai, f)
 def ai_chat(text:str):
+    with open('histori.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        chat_ai = lms.Chat.from_history(data)
+        file.close
     chat_ai.add_user_message(text)
     ai_text = str(model.respond(chat_ai))
+    new_message = {'role': 'user', 'content': text}
+    data['messages'].append(new_message)
+    new_message = {'role': 'assistant', 'content': ai_text}
+    data['messages'].append(new_message)
+    with open("histori.json", "w",encoding='utf-8') as f:
+        json.dump(data, f)
     return ai_text
 @app.route('/')
 def index():
